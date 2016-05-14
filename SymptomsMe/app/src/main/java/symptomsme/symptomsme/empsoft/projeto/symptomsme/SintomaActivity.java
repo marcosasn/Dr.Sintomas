fcb55177a9f7e6d33c20369dcbc356915f0d745f
marcosasn <marcos.nascimento@ccc.ufcg.edu.br>
2016-05-14 15:01:08 -0300

    Implementação da tela de sintoma e tela de diagnóstico.

--- SymptomsMe/app/src/main/java/symptomsme/symptomsme/empsoft/projeto/symptomsme/SintomaActivity.java
+++ SymptomsMe/app/src/main/java/symptomsme/symptomsme/empsoft/projeto/symptomsme/SintomaActivity.java
@@ -9,23 +9,13 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class SintomaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sintoma);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
